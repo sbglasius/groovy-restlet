2688b16e1f08dbd856c7a25150f4e0dc4b7ff43b
iamkeke@gmail.com <iamkeke@gmail.com@61b5d807-cd46-0410-9691-d376c6290b29>
2008-02-26 05:47:37 +0000

    rename to RestletBuilder

---
+++ groovy-restlet/src/main/java/org/lpny/gr/builder/RestletBuilder.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.lpny.gr.builder;
+
+import groovy.util.FactoryBuilderSupport;
+
+import org.lpny.gr.builder.factory.ApplicationFactory;
+import org.lpny.gr.builder.factory.ClientFactory;
+import org.lpny.gr.builder.factory.ComponentFactory;
+import org.lpny.gr.builder.factory.DirectoryFactory;
+import org.lpny.gr.builder.factory.GuardFactory;
+import org.lpny.gr.builder.factory.RedirectorFactory;
+import org.lpny.gr.builder.factory.ResourceFactory;
+import org.lpny.gr.builder.factory.RestletFactory;
+import org.lpny.gr.builder.factory.RouterFactory;
+import org.lpny.gr.builder.factory.ServerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Restlet Builder
+ * 
+ * @author keke
+ * @reversion $Revision$
+ * @since 0.1.0
+ * @see <a href="http://www.restlet.org">Restlet</a>
+ * @see <a href="http://groovy.codehaus.org/GroovyMarkup">Groovy Markup</a>
+ */
+public class RestletBuilder extends FactoryBuilderSupport {
+    private static final Logger LOG = LoggerFactory.getLogger(RestletBuilder.class);
+
+    public RestletBuilder() {
+        super();
+        registerFactories();
+    }
+
+    private void registerFactories() {
+        registerFactory("component", new ComponentFactory());
+        registerFactory("application", new ApplicationFactory());
+        registerFactory("restlet", new RestletFactory());
+        registerFactory("resource", new ResourceFactory());
+        registerFactory("router", new RouterFactory());
+        registerFactory("directory", new DirectoryFactory());
+        registerFactory("client", new ClientFactory());
+        registerFactory("server", new ServerFactory());
+        registerFactory("guard", new GuardFactory());
+        registerFactory("redirector", new RedirectorFactory());
+    }
+}
